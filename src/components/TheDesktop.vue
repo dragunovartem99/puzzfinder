21a539f97d1a17e28358d7f70f242e520711adf2
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-01 23:07:12 +0300

    Add some shortcuts

---
+++ src/components/TheDesktop.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import TheShortcut from "./TheShortcut.vue";
+const emit = defineEmits(["app-clicked"]);
+const shortcuts = [
+	{
+		label: "Puzzfinder",
+		onClick: () => emit("app-clicked"),
+		icon: "/logo.png"
+	},
+	{
+		label: "Lichess Accuracy",
+		link: "https://dragunovartem99.github.io/lichess-accuracy",
+		icon: "/shortcuts/lichess-accuracy.png"
+	},
+	{
+		label: "Vue PGN Viewer",
+		link: "https://dragunovartem99.github.io/vue-pgn-viewer",
+		icon: "/shortcuts/vue-pgn-viewer.png"
+	},
+	{
+		label: "HTML Diagram",
+		link: "https://dragunovartem99.github.io/html-diagram",
+		icon: "/shortcuts/html-diagram.png"
+	},
+];
+</script>
+
+<template>
+	<div class="desktop">
+		<div class="shortcuts">
+			<TheShortcut
+				v-for="shortcut of shortcuts"
+				@click="shortcut.onClick"
+				:icon="shortcut.icon"
+				:label="shortcut.label"
+				:link="shortcut.link"
+			/>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.desktop {
+	background-color: teal;
+	position: fixed;
+	height: 100vh;
+	width: 100%;
+	padding: 20px 10px;
+}
+.shortcuts {
+	display: inline-flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	gap: 10px;
+}
+.shortcuts > * {
+	width: 80px;
+}
+</style>
---
+++ src/components/TheShortcut.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{ icon: string; label: string, link?: string }>();
+</script>
+
+<template>
+	<a :href="link" target="_blank">
+		<img :src="icon">
+		<p>{{ label }}</p>
+	</a>
+</template>
+
+<style scoped>
+a {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+img {
+	width: 32px;
+}
+p {
+	color: white;
+	text-align: center;
+	font-size: 14px;
+}
+</style>
