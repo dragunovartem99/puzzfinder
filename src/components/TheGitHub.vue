2b1173e472d6e6a41642b88dc92a1dd89598982f
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-07 15:02:24 +0300

    Refactor the GitHub section

--- src/components/TheGitHub.vue
+++ src/components/TheGitHub.vue
@@ -1,20 +1,25 @@
+<script setup lang="ts">
+const components = [
+	{
+		link: "https://github.com/dragunovartem99/puzzfinder",
+		text: "Client-side web interface",
+	},
+	{
+		link: "https://github.com/dragunovartem99/puzzfinder-api",
+		text: "Application programming interface",
+	},
+	{
+		link: "https://github.com/dragunovartem99/puzzfinder-db",
+		text: "SQLite database with chess puzzles",
+	},
+];
+</script>
+
 <template>
 	<p>The project consists of three components:</p>
 	<ul>
-		<li>
-			<a href="https://github.com/dragunovartem99/puzzfinder" target="_blank">
-				Client-side web interface
-			</a>
-		</li>
-		<li>
-			<a href="https://github.com/dragunovartem99/puzzfinder-api" target="_blank">
-				Application programming interface
-			</a>
-		</li>
-		<li>
-			<a href="https://github.com/dragunovartem99/puzzfinder-db" target="_blank">
-				SQLite database with chess puzzles
-			</a>
+		<li v-for="component of components">
+			<a :href="component.link" target="_blank"> {{ component.text }} </a>
 		</li>
 	</ul>
 	<p>Any server-side advice/help will be very welcomed! This is my first full-stack project.</p>
