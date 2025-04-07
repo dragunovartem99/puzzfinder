0bedb973d20f1db9aa47910788bc6bacacbd48a9
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-07 15:23:34 +0300

    Refactor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import TheDesktop from "./components/TheDesktop.vue";
+import TheDesktop from "./components/TheDesktop/TheDesktop.vue";
 import TheWindow from "./components/TheWindow.vue";
 
 const isOpen = ref(true);
--- src/state/pagination.ts
+++ src/state/pagination.ts
@@ -3,4 +3,6 @@ import { ref } from "vue";
 export const pagination = ref({
 	page: 1,
 	limit: 24,
+	total: 0,
+	totalPages: 0,
 });
