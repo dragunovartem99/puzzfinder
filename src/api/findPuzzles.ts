e9d8ad0641787a6db80770fb92c102e843cffb12
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-11 22:55:13 +0300

    Update local IP

--- src/api/findPuzzles.ts
+++ src/api/findPuzzles.ts
@@ -2,7 +2,7 @@ import type { SearchPayload } from "../types";
 
 export async function findPuzzles(body: SearchPayload) {
 	const response = await fetch("https://puzzfinder.99x.space/puzzles", {
-		// const response = await fetch("http://192.168.0.103:50000/puzzles", {
+	// const response = await fetch("http://192.168.1.199:50000/puzzles", {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(body),
