16770b8ac58c3953446712b0129b0347cc939e01
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-14 19:58:43 +0300

    Remove code duplication

--- src/modules/main-window/actions/search.ts
+++ src/modules/main-window/actions/search.ts
@@ -3,27 +3,11 @@ import type { SearchForm, SearchPayload } from "../types";
 import { findPuzzles } from "../api/findPuzzles.ts";
 
 import { useSearchForm, usePagination, usePuzzles } from "../state";
+import { createSearchPayload } from "./createSearchPayload.ts";
 
 const { searchForm, setSearchForm } = useSearchForm();
 const { setApiPuzzles, setIsLoading } = usePuzzles();
 const { pagination, setApiPagination } = usePagination();
-
-function createSearchPayload() {
-	const { sort, filters } = searchForm.value;
-	const [sortField, sortOrder] = sort.split("-");
-
-	return {
-		filters: {
-			rating: { ...filters.rating },
-			movesNumber: { ...filters.movesNumber },
-			popularity: { ...filters.popularity },
-			nbPlays: { ...filters.nbPlays },
-			themes: [...filters.themes],
-		},
-		sort: { field: sortField, order: sortOrder },
-		pagination: { ...pagination.value },
-	};
-}
 
 async function findResults(payload: SearchPayload) {
 	try {
@@ -43,14 +27,14 @@ async function findResults(payload: SearchPayload) {
 export async function submitForm(newSearchForm: SearchForm) {
 	setSearchForm(newSearchForm);
 
-	const payload = createSearchPayload();
+	const payload = createSearchPayload({ searchForm, pagination });
 	payload.pagination.page = 1;
 
 	findResults(payload);
 }
 
 export async function changePage(newPage: number) {
-	const payload = createSearchPayload();
+	const payload = createSearchPayload({ searchForm, pagination });
 	payload.pagination.page = newPage;
 
 	findResults(payload);
